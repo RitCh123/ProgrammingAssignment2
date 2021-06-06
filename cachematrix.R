a96a6ce5c8670ca3ef03de5231dc7c37e2505771
RitCh123 <ritchavali@gmail.com>
2021-06-05 21:00:28 -0700

    Added comments for the descriptions for both makeCacheMatrix() function and cacheSolve() function. In addition, modified functionality to fit the assignment, instead of storing mean - storing inverse of matrix in cache (similar functionality)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#Similar to makeVector() function
+#makeCacheMatrix function
+#takes matrix x for input, assigns variable m to the cache 
+#functions to get value of matrix, set value of inverse matrix in cache
+#and get the value of inverse of matrix in cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  get <- function() x
+  setSolve <- function(matrixObject) m <<- matrixObject
+  getSolve <- function() m
+  list(get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
-
-
-## Write a short comment describing this function
-
+#cacheSolve function
+#takes parameter matrix x
+#gets inverse from cache, checks for existence
+#if so, get inverse from cache, if not
+#solve inverse and store in cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  if(!is.null(m)) {
+    message("Cache data from variable")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setSolve(m)
+  m
 }
